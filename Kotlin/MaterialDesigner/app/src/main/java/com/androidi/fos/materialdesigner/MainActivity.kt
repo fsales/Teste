5209b380053d0c8b36d5966c6169731982b35907
Fábio Sales <fabio.oliveira.sales@gmail.com>
2018-05-06 12:43:16 -0300

    adicionando navigation drawer

--- Kotlin/MaterialDesigner/app/src/main/java/com/androidi/fos/materialdesigner/MainActivity.kt
+++ Kotlin/MaterialDesigner/app/src/main/java/com/androidi/fos/materialdesigner/MainActivity.kt
@@ -2,6 +2,8 @@ package com.androidi.fos.materialdesigner
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -12,21 +14,45 @@ import org.jetbrains.anko.toast
 class MainActivity : AppCompatActivity() {
 
     private val SECOND_ACTIVITY_REQUEST_CODE = 9999
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        btn_tela_login.setOnClickListener {
-            startActivityForResult(
-                    Intent(this@MainActivity, TelaLoginActivity::class.java), SECOND_ACTIVITY_REQUEST_CODE)
+        // adicionando a navigation drawer
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.menu_login -> {
+                    startActivityForResult(
+                            Intent(this@MainActivity, TelaLoginActivity::class.java), SECOND_ACTIVITY_REQUEST_CODE)
+                    true
+                }
+                R.id.menu_botao -> {
+                    startActivityForResult(
+                            Intent(this@MainActivity, TelaBotoesActivity::class.java), SECOND_ACTIVITY_REQUEST_CODE)
+                    true
+                }
+                else -> false
+            }
+
+        }
+    }
+
+
+    override fun onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            return
         }
 
-        btn_tela_botao.setOnClickListener {
-            startActivityForResult(
-                    Intent(this@MainActivity, TelaBotoesActivity::class.java), SECOND_ACTIVITY_REQUEST_CODE)
-        }
-
+        super.onBackPressed()
     }
 
 
